6bf685313d9667426d54233ea7539e079daab447
SiddhantDevTJ <siddhantkumar@MacBook-Air.local>
2025-03-01 20:41:53 +0530

    fetched pokemon api

--- src/componenets/Pokemon.jsx
+++ src/componenets/Pokemon.jsx
@@ -1,7 +1,36 @@
-import React from "react";
+import { useEffect, useState } from "react";
+
 import "../index.css";
 
 export const Pokemon = () => {
+  const [pokemonData, setPokemonData] = useState([]);
+
+  const pokeApi = "https://pokeapi.co/api/v2/pokemon?limit=151";
+  //   const bool isLoading = true;
+
+  const fetchPokemon = async () => {
+    try {
+      const response = await fetch(pokeApi);
+      const data = await response.json();
+
+      const detailPokemonData = data.results.map(async (pokemon) => {
+        const response = await fetch(pokemon.url);
+        const data = await response.json();
+        // console.log(data);
+        return data;
+      });
+      const detailedResults = await Promise.all(detailPokemonData);
+      console.log(detailedResults);
+      setPokemonData(detailedResults);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchPokemon();
+  }, []);
+
   return (
     <div>
       <h1>Pokemon</h1>
